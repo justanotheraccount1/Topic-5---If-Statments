89837d3bd1e333ebb1297df1e3b842957c256e55
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-18 09:38:15 -0400

    Add project files.

---
+++ Topic 5 - If Statments/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Topic_5___If_Statments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sam Ahrens 09/18/24
+            int people = 20;
+            int cats = 30;
+            int dogs = 15;
+
+            Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
+            if (people < cats)
+                Console.WriteLine("Too amny cats! The world is DOOMED!");
+
+            if (people > cats)
+                Console.WriteLine("Not a lot of cats, the world is saved.");
+
+            if (people < dogs)
+                Console.WriteLine("The world is drooled on.");
+
+            if (people > dogs)
+                Console.WriteLine("The world is dry.");
+
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            dogs += 5;
+            Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
+            if (people >= dogs)
+                Console.WriteLine("People are greater than or equal to dogs.");
+
+            if (people <= dogs)
+                Console.WriteLine("People are less than or equal to dogs.");
+
+            if (people == dogs)
+                Console.WriteLine("People are dogs.");
+
+        }
+    }
+}
