02b9f82347a29895bbdc43fa844b8337306e0a2d
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-20 09:33:32 -0400

    Part 1 Finished

--- Topic 5 - If Statments/Program.cs
+++ Topic 5 - If Statments/Program.cs
@@ -5,36 +5,86 @@
         static void Main(string[] args)
         {
             //Sam Ahrens 09/18/24
+            string dinosaur, magicWord;
+            int age, confirm;
+            double temp;
             int people = 20;
-            int cats = 30;
+            int cats = 20;
             int dogs = 15;
 
-            Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
-            if (people < cats)
-                Console.WriteLine("Too amny cats! The world is DOOMED!");
+            //Part 1
+            //Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
+            //if (people < cats)
+            //    Console.WriteLine("Too amny cats! The world is DOOMED!");
 
-            if (people > cats)
-                Console.WriteLine("Not a lot of cats, the world is saved.");
+            //if (people > cats)
+            //    Console.WriteLine("Not a lot of cats, the world is saved.");
 
-            if (people < dogs)
-                Console.WriteLine("The world is drooled on.");
+            //if (people < dogs)
+            //    Console.WriteLine("The world is drooled on.");
 
-            if (people > dogs)
-                Console.WriteLine("The world is dry.");
+            //if (people > dogs)
+            //    Console.WriteLine("The world is dry.");
 
-            Console.WriteLine("Press ENTER to continue...");
-            Console.ReadLine();
-            Console.Clear();
-            dogs += 5;
-            Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
-            if (people >= dogs)
-                Console.WriteLine("People are greater than or equal to dogs.");
+            //Console.WriteLine("Press ENTER to continue...");
+            //Console.ReadLine();
+            //Console.Clear();
+            //dogs += 5;
+            //Console.WriteLine("People:" + people + " Cats:" + cats + " Dogs:" + dogs);
+            //if (people >= dogs)
+            //    Console.WriteLine("People are greater than or equal to dogs.");
 
-            if (people <= dogs)
-                Console.WriteLine("People are less than or equal to dogs.");
+            //if (people <= dogs)
+            //    Console.WriteLine("People are less than or equal to dogs.");
 
-            if (people == dogs)
-                Console.WriteLine("People are dogs.");
+            //if (people == dogs)
+            //    Console.WriteLine("People are dogs.");
+
+            //Console.WriteLine("What famous dinosaur has 3 horns?");
+            //dinosaur = Console.ReadLine();
+            //if (dinosaur.ToLower() == "triceratops")
+            //    Console.WriteLine("Correct!");
+
+
+            //Console.WriteLine("What's the magic word?");
+            //magicWord = Console.ReadLine().ToLower();
+            //if (magicWord == "please")
+            //    Console.WriteLine("Correct!");
+            //else
+            //    Console.WriteLine("Nope that's not the magic word");
+            //if (magicWord == "what")
+            //    Console.WriteLine("But sort of correct?");
+
+
+            //Console.WriteLine("How old are you?");
+            //int.TryParse(Console.ReadLine(), out age);
+            //if (age <= 0)
+            //    Console.WriteLine("How are you even typing?");
+            //if (age <= 15)
+            //    Console.WriteLine("You can't drive");
+            //if (age <= 17)
+            //    Console.WriteLine("You can't vote");
+            //if (age <= 24)
+            //    Console.WriteLine("You can't rent a car");
+            //if (age >= 25)
+            //    Console.WriteLine("You can do anything legal");
+
+            //Console.WriteLine("Please enter a freezing temperature of water:");
+            //double.TryParse(Console.ReadLine(), out temp);
+            //if (temp == 0)
+            //    Console.WriteLine("Yes. 0 degrees Celcius is the exact freezing temperature of water");
+            //if (temp == 32)
+            //    Console.WriteLine("Yes. 32 degrees Ferenhiet is the exact freezing temperature of water");
+            //if (temp == 273.2)
+            //    Console.WriteLine("Yes. 273.2 degrees Kelvin is the exact freezing temperature of water");
+
+
+            //Part 2
+            
+
+
+
+
 
         }
     }
